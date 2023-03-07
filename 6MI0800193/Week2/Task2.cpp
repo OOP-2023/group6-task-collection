0c90fa694ba0496e26d4ac95caf671af9c47e19b
stenlik26 <stenli_karev@abv.bg>
2023-03-07 12:59:58 +0200

    Week2 update

--- 6MI0800193/Week2/Task2.cpp
+++ 6MI0800193/Week2/Task2.cpp
@@ -6,13 +6,12 @@ size_t chars_in_file(const char* file)
     size_t chars = 0;
     ifstream input_file(file);
 
-    while (true)
+    if (!input_file.is_open())
+        return chars;
+
+    for (char c = input_file.get(); c >= 0; c = input_file.get())
     {
-        char c = input_file.get();
-
-        if (input_file.eof()) break;
-
-        if (c != 'a' && c != 'd' && c!= ' ' && c!='\n')
+        if (c != 'a' && c != 'd' && c != ' ' && c != '\n')
             chars++;
     }
 
--- 6MI0800193/Week2/Task5.cpp
+++ 6MI0800193/Week2/Task5.cpp
@@ -2,7 +2,20 @@
 #include <fstream>
 using namespace std;
 
+struct User 
+{
+	char name[32];
+	char password[32];
+	char email[32];
+
+};
+
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	User users[100];
+	size_t users_count = 0;
+
+
 }
