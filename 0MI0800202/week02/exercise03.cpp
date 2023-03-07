ee3ced33e5688192636e595013fe198a91e778c0
swordpepi <peter.hristov03@abv.bg>
2023-03-07 12:59:05 +0200

    change folder name

---
+++ 0MI0800202/week02/exercise03.cpp
@@ -0,0 +1,17 @@
+struct Pair
+{
+    int a, b;
+};
+Pair createPair(int x, int y)
+{
+    Pair pair{};
+    return pair;
+}
+void initPair(pair& toInit, int x, int y)
+{
+
+}
+int main()
+{
+
+}
---
+++ 0MI0800202/week02/testfile.cpp
@@ -0,0 +1,19 @@
+//ifstream file("result.txt");
+//
+//if (!file.is_open())
+//{
+//cout << "Error!" << endl;
+//}
+//char text[MAX_SIZE];
+//file.get(text, MAX_SIZE);
+//int counter = 0;
+//char* textPtr = text;
+//while (textPtr[0] != '\0')
+//{
+//if (textPtr[0] != 'a' && textPtr[0] != 'd' && textPtr[0] != ' ')
+//{
+//counter++;
+//}
+//textPtr++;
+//}
+//return counter;
