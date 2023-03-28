d4d5a72b942854b3ab0a18d7bd3495279f2c3413
GitHubDiligentUser <118621719+GitHubDiligentUser@users.noreply.github.com>
2023-03-28 12:55:33 +0300

    Add files via upload

---
+++ 1MI0800224/week5/28.03_zad3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+struct Jobangebot{
+    char firmenname[26];
+    unsigned count_programmer;
+    unsigned out_days;
+    double verdienst;
+};
+//out of time
+int main(){
+}
