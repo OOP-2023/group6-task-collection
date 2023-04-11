5f931b9f4fc5db2617b2c3a26f16d9bc28f6e396
Kaloyantr <115221551+Kaloyantr@users.noreply.github.com>
2023-04-11 12:31:43 +0300

    Add files via upload

---
+++ zad1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+template<typename T>
+const T& max(T* arr, size_t size)
+{
+	if (size == 0)
+		throw std::exception();
+	size_t maxIndex = 0;
+	for (size_t i = 1; i < size; i++)
+	{
+		if (arr[maxIndex] < arr[i])
+			arr[maxIndex] = arr[i];
+	}
+	return arr[maxIndex];
+}
+
+//int main()
+//{
+//	int* arr = new int[10];
+//
+//	for (size_t i = 0; i < 10; i++)
+//	{
+//		arr[i] = rand();
+//	}
+//
+//	std::cout << max<int>(arr, 10);
+//}
+
+
